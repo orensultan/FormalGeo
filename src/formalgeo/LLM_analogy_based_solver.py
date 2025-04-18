1c98327b92bf111e6f6923c2a964fc44d0aea9da
orensultan <osultan@lightricks.com>
2025-04-18 15:19:01 +0300

    level 3 random problem 844 error in parsing

--- src/formalgeo/LLM_analogy_based_solver.py
+++ src/formalgeo/LLM_analogy_based_solver.py
@@ -458,20 +458,20 @@ def get_level_to_problems(problems):
     return level_to_problems
 
 
-chosen_problems_by_level = {
-    # 1: [2833],
-    # 2: [6523],
-    # 3: [2999],
-    # 4: [2425],
-    # 5: [4908],
-    # 6: [729],
-    #  7: [683],
-    # 8: [912],
-    # 9: [5749]
-}
+# chosen_problems_by_level = {
+#     # 1: [2833],
+#     # 2: [6523],
+#     # 3: [2999],
+#     # 4: [2425],
+#     # 5: [4908],
+#     # 6: [729],
+#     #  7: [683],
+#     # 8: [912],
+#     # 9: [5749]
+# }
 
 chosen_problems_by_level = {
-    3: [4254]
+     3: [844]
     # 1: [1975, 1490, 1726, 178, 2669, 2614, 51, 2323, 192, 2624],
     # 2: [991, 69, 144, 358, 4473, 4483, 5645, 127, 2410, 4523],
     # 3: [ 4187, 5244, 5062, 844, 1945, 2200, 4099, 2765, 4476, 4254 ]
@@ -660,7 +660,7 @@ def run_theorems_coverage(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--variant", dest="variant", type=str, default="analogy_based")
+    parser.add_argument("--variant", dest="variant", type=str, default="random_all_theorems")
     parser.add_argument("--model_name", dest="model_name", type=str, default="o1")
     parser.add_argument("--prompt_path", dest="prompt_path", type=str,
                         default="src/formalgeo/prompt/geometry_similar_problems_prompt_291224.txt")
